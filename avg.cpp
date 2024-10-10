05afac842670a0307cb89c360c532626e39020ea
JeremyQuintana5 <jeremy.quintana@upr.edu>
2024-10-10 14:11:36 +0000

    Almost

---
+++ avg.cpp
@@ -0,0 +1,21 @@
+// JeremyQuintana5
+
+#include <iostream>
+#include <format>
+
+using namespace std;
+int main() {
+  int n;
+  int ctr = 0;
+
+cout << "Entra 10 enteros: ";
+  for (int i = 0; i < 10; i++) {
+    cin >> n;
+    sum += n;
+  }
+
+double average = sum / 10.0
+cout << format("Promedio: {:.2f}\n", average);
+
+return 0;
+}
